db47b687fb1077647ebfbe37c99650da79c9cd02
Szymon Węgrzynek <u105_szyweg_waw@technischools.com>
2023-11-27 20:17:19 +0100

    first commit

---
+++ src/components/Carouselle/Carouselle.jsx
@@ -0,0 +1,69 @@
+import Carousel from "react-elastic-carousel";
+import "./Carouselle.scss";
+
+const Carouselle = (props) => {
+  const { imageUrl, name } = props;
+
+  const handleCarouselClick = (e) => {
+    e.stopPropagation();
+  };
+
+  return (
+    <div className="carousel-container" onClick={handleCarouselClick}>
+      <Carousel
+        autoPlaySpeed={4000}
+        enableAutoPlay={true}
+        showArrows={false}
+        focusOnSelect={true}
+        pagination={true}
+      >
+        <div className="carousel-item">
+          <img src={imageUrl} alt={name} />
+          <p>{name}</p>
+        </div>
+        <div className="carousel-item">
+          <img src={imageUrl} alt={name} />
+          <p>{name}</p>
+        </div>
+        <div className="carousel-item">
+          <img src={imageUrl} alt={name} />
+          <p>{name}</p>
+        </div>
+        <div className="carousel-item">
+          <img src={imageUrl} alt={name} />
+          <p>{name}</p>
+        </div>
+        <div className="carousel-item">
+          <img src={imageUrl} alt={name} />
+          <p>{name}</p>
+        </div>
+        <div className="carousel-item">
+          <img src={imageUrl} alt={name} />
+          <p>{name}</p>
+        </div>
+        <div className="carousel-item">
+          <img src={imageUrl} alt={name} />
+          <p>{name}</p>
+        </div>
+        <div className="carousel-item">
+          <img src={imageUrl} alt={name} />
+          <p>{name}</p>
+        </div>
+        <div className="carousel-item">
+          <img src={imageUrl} alt={name} />
+          <p>{name}</p>
+        </div>
+        <div className="carousel-item">
+          <img src={imageUrl} alt={name} />
+          <p>{name}</p>
+        </div>
+        <div className="carousel-item">
+          <img src={imageUrl} alt={name} />
+          <p>{name}</p>
+        </div>
+      </Carousel>
+    </div>
+  );
+};
+
+export default Carouselle;
---
+++ src/components/ProductModal/ProductModal.jsx
@@ -0,0 +1,71 @@
+import "./ProductModal.scss";
+import { FaArrowLeftLong } from "react-icons/fa6";
+import { FaRegHeart } from "react-icons/fa";
+import Carouselle from "../Carouselle/Carouselle";
+
+const ProductModal = (props) => {
+  const {
+    product,
+    onClose,
+    onLike,
+    isOrdered,
+    orderCount,
+    handleButtonClick,
+    orderedProducts,
+  } = props;
+  const { imageUrl, description, name, price } = product;
+
+  const handleCloseButtonClick = () => {
+    onClose();
+  };
+
+  return (
+    <div className="product-modal">
+      <div className="product-modal-inner">
+        <button className="add-to-fav">
+          <FaRegHeart />
+        </button>
+        <button className="close-btn" onClick={handleCloseButtonClick}>
+          <FaArrowLeftLong />
+        </button>
+        <div className="product-modal-part">
+          <div className="all-without-carousel">
+            <div className="div-with-img">
+              {" "}
+              <img src={imageUrl} alt={name} />
+            </div>
+            <div className="div-with-ndp">
+              {" "}
+              <h4>{name}</h4>
+              <p>{description}</p>
+              <strong className="price">{price}</strong>
+            </div>
+          </div>
+          <div className="carouselle-part">
+            <p>DODAJ TO CO LUBISZ</p>
+            <Carouselle></Carouselle>
+          </div>
+          <div className="add-to-basket-part">
+            <div className="add-count-to-basket">
+              <button type="button">-</button>
+              <span>{}</span>
+              <button type="button">+</button>
+            </div>
+            <div className="add-to-basket-button">
+              <button
+                type="button"
+                onClick={handleButtonClick}
+                isOrdered={isOrdered}
+                orderCount={orderCount}
+              >
+                {" "}
+                {isOrdered ? orderCount : "+"}
+              </button>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};-
+export default ProductModal;
